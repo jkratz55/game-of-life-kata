1203784d4c01d21197c583f31bc6fe6196ae3373
Joseph Kratz <joseph.kratz06@gmail.com>
2018-12-20 23:16:46 -0500

    Added missing files for Scala solution

---
+++ scala/build.sbt
@@ -0,0 +1,9 @@
+name := "game-of-life-kata"
+
+version := "1.0-SNAPSHOT"
+
+scalaVersion := "2.12.8"
+
+libraryDependencies ++= Seq(
+  "org.scalatest" % "scalatest_2.12" % "3.0.5"
+)
---
+++ scala/src/test/scala/io/jkratz/katas/gameoflife/LifeSpec.scala
@@ -0,0 +1,12 @@
+package io.jkratz.katas.gameoflife
+
+import org.scalatest.{FunSpec, Matchers}
+
+class LifeSpec extends FunSpec with Matchers {
+
+  val board = Board.random(5,5)
+
+  describe("For a single tick") {
+    println("fjosaefhesoifgheshgohesoighoiesjfoesifjoes")
+  }
+}
