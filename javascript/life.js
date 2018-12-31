239f77a58cfe8813c037d16e1b371182115bc43d
Joseph Kratz <joseph.kratz06@gmail.com>
2018-12-30 19:00:17 -0500

    Added JS version

---
+++ javascript/life.js
@@ -0,0 +1,10 @@
+class Board {
+
+    constructor(columns = 0, rows = 0) {
+        this.columns = columns;
+        this.rows = rows;
+        console.log("I am a teapot")
+    }
+}
+
+var board = new Board(10,10);
